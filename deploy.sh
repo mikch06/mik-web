32ac797d3721662b63cd9455ad54a72757438cea
mik <mail@kissel.ch>
2024-12-09 21:49:14 +0100

    line breaks in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #/bin/bash
 # Generate search Index
 # Deploy to prod
-./hugo/assets/pagefind/pagefind --site hugo/public/ && cd hugo && hugo -b https://kissel.ch/ict && rsync -avz public/ mikcyons@kissel.ch:/home/mikcyons/www/kissel.ch/ict
+
+#set -x
+./hugo/assets/pagefind/pagefind --site hugo/public/\
+&& cd hugo && hugo -b https://kissel.ch/ict\
+&& rsync -avz public/ mikcyons@kissel.ch:/home/mikcyons/www/kissel.ch/ict
